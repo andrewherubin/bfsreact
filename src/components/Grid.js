a934df4abfb5fc324436163f41305f9df0cfcfb0
Andrew Herubin <andrewherubin@outlook.com>
2022-03-15 22:00:22 -0500

    modified boiler plate, added header began development of grid

---
+++ src/components/Grid.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class Grid extends Component {
+constructor(){
+    super();
+    this.state={
+        table: []
+    };
+}
+
+  render() {
+    return (
+      <div>Grid</div>
+    )
+  }
+}
+
+export default Grid
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-dark text-light text-center'>
+        <h1 className='p-5'>Breadth First Search Visualizer</h1>
+    </header>
+  )
+}
+
+export default Header
